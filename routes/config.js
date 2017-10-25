3dbf3372ad901af8046ed4c9ee9f40bced2ad366
Grant Hernandez <grant.h.hernandez@gmail.com>
2017-10-25 15:10:39 -0400

    Add ability to change profile information

--- routes/config.js
+++ routes/config.js
@@ -1,4 +1,5 @@
 'use strict';
+
 (function() {
 	// Module "myModule" is created here
 	let app = angular.module('myModule', ['ngRoute', 'pascalprecht.translate']);
@@ -16,30 +17,44 @@
 		.when('/login', {
 			templateUrl: 'login.html',
 			controller: 'LoginController',
-			controllerAs: 'main',
+			controllerAs: 'login',
 		})
 		.when('/register', {
 			templateUrl: 'register.html',
 			controller: 'RegisterController',
-			controllerAs: 'main',
+			controllerAs: 'register',
 		})
 		.when('/admin', {
 			templateUrl: 'admin.html',
 			controller: 'AdminController',
-			controllerAs: 'main',
+			controllerAs: 'admin',
 		})
 		.when('/home', {
 			templateUrl: 'views/home.html',
 			controller: 'homeController',
 			controllerAs: 'home',
 		})
-		//	.when("/meeting_signin", {
-		//		templateUrl: "views/meeting_signin.html",
-		//		controller: "meetingController",
-		//		controllerAs: "meeting"
-		//	})
+		.when('/profile', {
+			templateUrl: 'views/profile.html',
+			controller: 'ProfileController',
+			controllerAs: 'profile',
+			resolve: {
+				profile_data: function($http) {
+					return $http.get('/api/user/profile').then(function(response) {
+						return response.data;
+					});
+				},
+			},
+		})
 		.otherwise({redirectTo: '/'});
 
 		console.log(navigator.language);
 	}
+
+	app.run(function($rootScope, $location) {
+		$rootScope.$on('$routeChangeError', function(event, current, previous, eventObj) {
+			console.log(eventObj);
+			$location.path('/');
+		});
+	});
 }());
