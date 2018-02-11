9c6a5d6a22c47bb264ff45ac433bc9e1de94db80
Joseph Komskis <joekom0725@ufl.edu>
2018-02-11 17:27:02 -0500

    Fixed table loading with a janky solution.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -18,21 +18,22 @@ export class AdminComponent implements OnInit {
     this.requests.user_list('/list_users').subscribe(
       // This portion of code is run if the list is properly returned
       res => {
-        for (let i = 0; i < res.length; ++i) {
+        // TODO: FIX JANKINESS
+        let i = 0;
+        while (res[i] != undefined) {
           let user = res[i];
           user['mass_mail_optin'] = (user['mass_mail_optin'] === 1) ? 'Yes' : 'No';
-          /*if (user['mass_mail_optin'] == 1) {
-            user['mass_mail_optin'] = 'Yes';
-          }
-          else {
-            user['mass_mail_optin'] = 'No';
-          }*/
+          ++i;
         }
         this.users = res;
       },
       error => {    // This portion of code is run when there was an error retrieving the user list
 
       });
+  }
+
+  public getUsers() {
+    return this.users;
   }
 
 }
