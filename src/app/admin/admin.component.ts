4e72beea38c5366dea4204d6b0260ad062e9ff44
Joseph Komskis <joekom0725@ufl.edu>
2018-02-11 17:14:36 -0500

    Fixed displaying table. Only works with dev build. Cannot build production build.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -9,26 +9,28 @@ import { RestService } from '../rest.service';
 
 
 export class AdminComponent implements OnInit {
+  private users;
 
   constructor(private requests: RestService) { }
 
   ngOnInit() {
-    //Uses the RestService to make an http request for a list of users
-    this.requests.user_list("/list_users").subscribe(
-      //This portion of code is run if the list is properly returned
+    // Uses the RestService to make an http request for a list of users
+    this.requests.user_list('/list_users').subscribe(
+      // This portion of code is run if the list is properly returned
       res => {
-        let user: {}[];
-          res['list_users'].forEach(
-            user => {
-              if (user['mass_mail_optin'] == 1) {
-                user['mass_mail_optin'] = "Yes"                
-              }
-              else
-                user['mass_mail_optin'] = "No"
-            }
-          );
+        for (let i = 0; i < res.length; ++i) {
+          let user = res[i];
+          user['mass_mail_optin'] = (user['mass_mail_optin'] === 1) ? 'Yes' : 'No';
+          /*if (user['mass_mail_optin'] == 1) {
+            user['mass_mail_optin'] = 'Yes';
+          }
+          else {
+            user['mass_mail_optin'] = 'No';
+          }*/
+        }
+        this.users = res;
       },
-      error => {    //This portion of code is run when there was an error retrieving the user list
+      error => {    // This portion of code is run when there was an error retrieving the user list
 
       });
   }
