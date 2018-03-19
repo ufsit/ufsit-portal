b19d671edff14b4b9b519fb828a325a4559731b8
Spencer Flitter <spencerflitter97@gmail.com>
2018-03-18 20:21:50 -0400

    Cleaned up admin test-page for later use

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -5,11 +5,22 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { TimeAgoPipe } from '../time-ago.pipe';
 import { RestService } from '../rest.service';
 import { Observable } from 'rxjs/Observable';
-import { HttpResponse, HttpClient } from '@angular/common/http';
+import { HttpResponse, HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { of } from 'rxjs/observable/of';
 import {MockBackend, MockConnection} from '@angular/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SessionService } from '../session.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 class MockRestService {
+  public user_list(x) {
+    return of([{id: 1, email: "jd@ufl.edu", full_name: "John Doe", mass_mail_optin: "Yes", grad_date: "2018 (Spring)", 
+    registration_date: "2018-02-10T00:39:24.000Z"}]);
+  }
+}
+
+class MockSessionService {
+  
 }
 
 describe('AdminComponent', () => {
@@ -24,9 +35,12 @@ describe('AdminComponent', () => {
         AdminComponent ],
       imports: [
         RouterTestingModule,
-      ],
+        FormsModule,
+        ReactiveFormsModule,
+        NgbModule.forRoot()   ],
       providers: [
-        {provide: RestService, useClass: MockRestService}
+        {provide: RestService, useClass: MockRestService},
+        {provide: SessionService, useClass: MockSessionService}
       ]
     })
     .compileComponents();
