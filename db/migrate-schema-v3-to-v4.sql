52d12c028ef120671bc6464ca504696312c6ce75
Joseph Komskis <joekom0725@ufl.edu>
2018-04-04 17:47:22 -0400

    Added my database changes before deployment 2.

---
+++ db/migrate-schema-v3-to-v4.sql
@@ -0,0 +1,11 @@
+--Updated writeups to use ids
+ALTER TABLE `writeup_submissions` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
+--ALTER TABLE `writeup_submissions` DROP COLUMN `key`;
+ALTER TABLE `writeup_submissions` ADD `time_created` DATETIME NOT NULL;
+ALTER TABLE `writeup_submissions` ADD `time_updated` DATETIME NOT NULL;
+ALTER TABLE `writeup_submissions` ADD `name` varchar(255) NOT NULL DEFAULT '';
+ALTER TABLE `file_uploads` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
+--ALTER TABLE `file_uploads` DROP COLUMN `key`;
+ALTER TABLE `file_uploads` ADD `time_created` DATETIME NOT NULL;
+ALTER TABLE `file_uploads` ADD `name` varchar(255) NOT NULL DEFAULT '';
+
