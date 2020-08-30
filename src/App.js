b55b2d14e0e0eaf0cdaeeafdf2d9592ddc7606dc
SmoothHacker <laglerscott@gmail.com>
2020-08-30 13:49:36 -0500

    Create Login Component
    Functionality is still needed to be created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Sidebar from './components/sidebar.component';
+//import Sidebar from './components/sidebar.component';
 import Login from './components/login.component';
 
 function App() {
